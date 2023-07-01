781d5abfc8d87a0c2d7ed92cb9e6d53e0a16339f
sotiriosc <chortogiannoss@gmail.com>
2023-07-01 19:09:42 -0400

    attemptiong to fix user creation problem

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/balancedb
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
   }
 );
 
