b2222df39a472fb82881580789fe9abc1f0724d0
sotiriosc <chortogiannoss@gmail.com>
2023-07-02 08:56:56 -0400

    erased mistake

--- server/models/Category.js
+++ server/models/Category.js
@@ -14,12 +14,3 @@ const Category = mongoose.model('Category', categorySchema);
 
 module.exports = Category;
 
-
-{
-  "name": "Buy me a Coffee",
-  "description": "Show your support by buying me a coffee!",
-  "image": "https://images.unsplash.com/photo-1509042239860-f550ce710b93",
-  "price": 3.00,
-  "quantity": 100000,
-  "category": "64a17036a4fd44a00243ab01"
-}
