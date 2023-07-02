349937c891637a48db325abcd345db9b669a270c
sotiriosc <chortogiannoss@gmail.com>
2023-07-01 22:34:04 -0400

    debugging sw issue

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,7 @@ root.render(
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     // Register the service worker
-    navigator.serviceWorker.register('../public/sw').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
